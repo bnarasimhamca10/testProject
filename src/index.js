1e580f22a4acf53cc8d23806a4c49549498857ad
Bolagoni Narasimha Rao <bnarasimhamca10@gmail.com>
2021-12-24 14:52:57 +0530

    Adding loaders

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+ReactDOM.render(
+  <h1>Hello fromjs</h1>,
+  document.querySelector(
+    '#fromjs'
+  )
+);
+
